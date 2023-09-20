5e5e76ca13baa5a87a9c78f70f56ae006d058ceb
MSreeN <khyathimnsree369@gmail.com>
2023-09-20 21:17:59 +0530

    added pace and speed methods

--- script.js
+++ script.js
@@ -13,6 +13,46 @@ const inputElevation = document.querySelector('.form__input--elevation');
 
 let map, mapEvent;
 
+class Workout {
+  date = new Date();
+  id = (new Date().getTime() + ' ').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
+  }
+}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.calcSpeed();
+  }
+
+  calcSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+
+const run1 = new Running([1, 2], 5, 10, 5);
+const cyc1 = new Cycling([1, 2], 5, 10, 5);
+console.log(run1, cyc1);
+
+///////////////Application architecture;
 class App {
   #map;
   #mapEvent;
