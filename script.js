1ed8f6d20f6711454e698fdacb84fb57da7fdd8e
MSreeN <khyathimnsree369@gmail.com>
2023-09-21 16:57:04 +0530

    added validation to form

--- script.js
+++ script.js
@@ -95,10 +95,10 @@ class App {
     // console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
     //adding click event on map
-    this.#map.on('click', this._shoeForm.bind(this));
+    this.#map.on('click', this._showForm.bind(this));
   }
 
-  _shoeForm(mapE) {
+  _showForm(mapE) {
     console.log(this);
     this.#mapEvent = mapE;
     form.classList.remove('hidden');
@@ -113,12 +113,39 @@ class App {
 
   _newWorkout(e) {
     e.preventDefault();
-    //clearing input fields
-    inputCadence.value = '';
-    inputDistance.value = '';
-    inputDuration.value = '';
-    inputElevation.value = '';
 
+    const validInputs = (...inputs) => inputs.every(ip => Number.isFinite(ip));
+
+    const allPositive = (...inputs) => inputs.every(ip => ip > 0);
+
+    //Get data from form
+    const type = inputType.value;
+    const distance = inputDistance.value;
+    const duration = inputDuration.value;
+
+    //Check if data is valid
+
+    //If activity running, create running object
+    if (type == 'running') {
+      const cadence = +inputCadence.value;
+      if (
+        !validInputs(distance, duration, cadence) ||
+        !allPositive(distance, duration, cadence)
+      )
+        return alert('Inputs has to be number');
+    }
+    //If activity cycling, create cycling object
+    if (type == 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !validInputs(distance, duration, elevation) ||
+        !allPositive(distance, duration)
+      )
+        return alert('Inputs has to be number');
+    }
+    // Add new object to workout array
+
+    //Render workout on map as marker
     const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -131,6 +158,13 @@ class App {
       )
       .setPopupContent('workout')
       .openPopup();
+
+    //Hide form and clear input fields
+    //clearing input fields
+    inputCadence.value = '';
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputElevation.value = '';
   }
 }
 
